a78f4da5be167375ed4b8fc8355a69f7f832578f
aminta5 <fizi2u@yahoo.com>
2019-01-22 22:25:21 +0100

    initial project

---
+++ src/main/java/com/medialab/mybooks/MyBooksApplication.java
@@ -0,0 +1,14 @@
+package com.medialab.mybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBooksApplication.class, args);
+    }
+
+}
+
